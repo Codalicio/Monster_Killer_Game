469bd0d816b4efd73cefe08b06d67df3c026e8e5
Amit Raj <rajamitnine@gmail.com>
2025-11-04 19:29:09 +0530

    Add a 'click' eventListener to the STRONG ATTACK button and call strongAttackHandler on every click

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 11;
+const STRONG_ATTACK_VALUE = 15;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -21,4 +22,19 @@ function attackHandler() {
   }
 }
 
+const strongAttackHandler = () => {
+  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("It's a draw!");
+  }
+};
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
